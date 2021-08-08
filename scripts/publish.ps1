d5376d9b5899827e4e8616005c538a240c9d9d96
efeozyer <efeozyer@yandex.com>
2021-08-08 13:25:11 +0300

    Script & workflow changes

---
+++ scripts/publish.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $source,
+    
+    [Parameter(Mandatory = $true)]
+    [string]
+    $apiKey
+)
+
+$folder = '.\packages'
+
+if(Test-Path -Path $folder) 
+{
+    foreach($file in Get-ChildItem -Path .\packages)
+    {
+        dotnet nuget push .\packages\$file --source $source --api-key $apiKey --skip-duplicate
+    }   
+}
